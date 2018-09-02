927399f3ea540f0378098c34ea5ee77bf0f6df3c
a069buzz <42826229+a069buzz@users.noreply.github.com>
2018-09-02 23:01:58 +1000

    Delete the non working code and make it compact

--- Ass2/RectangularPrism.cpp
+++ Ass2/RectangularPrism.cpp
@@ -26,9 +26,7 @@ RectangularPrism::RectangularPrism()
 
 RectangularPrism::RectangularPrism(double x_, double y_, double z_, double width_, double length_, double height_)
 	: Shape(x_, y_, z_) {
-		blue = .5;
-		green = .5;
-
+		
 		width = width_;
 		length = length_;
 		height = height_;
@@ -36,51 +34,10 @@ RectangularPrism::RectangularPrism(double x_, double y_, double z_, double width
 
 void RectangularPrism::draw()
 {
-	double half_width = length / 2;
+	double half_width = width / 2;
 	length = length / 2;
 	double half_height = height / 2;
-	/*
-	float rectangular_vertices[] =
-	{
-		// Top 
-		x + half_width, y + height, z + length,
-		x - half_width, y + height, z + length,
-		x - half_width, y + height, z - length,
-		x + half_width, y + height, z - length,
-		// left
-		x - half_width, y + height, z + length,
-		x - half_width, y + height, z - length,
-		x - half_width, y  , z + length,
-		x - half_width, y  , z - length,
-		// right
-		x + half_width, y + height, z + length,
-		x + half_width, y + height, z - length,
-		x + half_width, y, z + length,
-		x + half_width, y, z - length,
-		// bottom
-		x + half_width, y, z + length,
-		x + half_width, y , z - length,
-		x - half_width, y , z + length,
-		x - half_width, y , z - length,
-		// front
-		x + half_width, y + half_height, z - length,
-		x + half_width, y , z - length,
-		x - half_width, y + half_height, z - length,
-		x - half_width, y , z - length,
-		// back
-		x + half_width, y + half_height, z + length,
-		x + half_width, y, z + length,
-		x - half_width, y + half_height, z + length,
-		x - half_width, y, z + length
-
-	};
-
-	glBegin(GL_QUAD_STRIP);
-		glColor3f(Shape::getRed(), Shape::getGreen(), Shape::getBlue);
-		glVertex3dv(rectangular_vertices);
-	glEnd();
-	*/
-
+	
 	//make 2 faces, make all the sides linking the faces
 
 	//first make 2 faces with displacement = length
@@ -90,30 +47,35 @@ void RectangularPrism::draw()
 		glVertex3d(x - half_width, y + height, z - length);
 		glVertex3d(x + half_width, y + height, z - length);
 	glEnd();
+	
 	glBegin(GL_QUADS);
 		glVertex3d(x +  half_width, y, z + length); //bottom face
 		glVertex3d(x - half_width, y, z + length);
 		glVertex3d(x - half_width, y, z - length);
 		glVertex3d(x + half_width, y, z - length);
 	glEnd();
+	
 	glBegin(GL_QUADS);
 		glVertex3d(x - half_width, y, z - length); //left face
 		glVertex3d(x - half_width, y, z + length);
 		glVertex3d(x - half_width, y + height, z + length);
 		glVertex3d(x - half_width, y + height, z - length);
 	glEnd();
+	
 	glBegin(GL_QUADS);
 		glVertex3d(x + half_width, y, z - length); //right face
 		glVertex3d(x + half_width, y, z + length);
 		glVertex3d(x + half_width, y + height, z + length);
 		glVertex3d(x + half_width, y + height, z - length);
 	glEnd();
+	
 	glBegin(GL_QUADS);
 		glVertex3d(x - half_width, y + height, z - length); //front face
 		glVertex3d(x - half_width, y, z - length);
 		glVertex3d(x + half_width, y, z - length);
 		glVertex3d(x + half_width, y + height, z - length);
 	glEnd();
+	
 	glBegin(GL_QUADS);
 		glVertex3d(x - half_width, y + height, z + length); //back face
 		glVertex3d(x - half_width, y, z + length);
